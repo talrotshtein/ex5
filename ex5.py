3c16a0dc17fd64036e8771cc242b2147b2f6ccd5
shahar.ben <shahar.ben@campus.technion.ac.il>
2022-06-25 16:48:49 +0300

    finished func 1 and started 2

--- ex5.py
+++ ex5.py
@@ -7,9 +7,9 @@ def names_of_registered_students(input_json_path, course_name):
     names = []
     with open(input_json_path, 'r') as file:
         source = json.load(file)
-    for i in source:
-        if course_name in ['registered_courses']:
-            names += ['student_name']
+        for course_data in source.values():
+            if course_name in course_data['registered_courses']:
+                names.append(course_data['student_name'])
     return names
 
 
@@ -17,14 +17,17 @@ def names_of_registered_students(input_json_path, course_name):
 
 
 def enrollment_numbers(input_json_path, output_file_path):
-    """
-    This function writes all the course names and the number of enrolled
-    student in ascending order to the output file in the given path.
+    num_of_students = 0
+    course_names = []
+    with open(input_json_path, 'r') as file:
+        source = json.load(file)
+        for course_data in source.values():
+            for course_data['registered_courses'] in course_data:
+                if course_data['registered_courses'] != course_names: #not sure about that
+                    course_names.append(course_data['registered_courses'])#not sure about that
+                    sorted(course_names)
+                    with open(output_file_path, 'w') as wfile:
 
-    :param input_json_path: Path of the students database json file.
-    :param output_file_path: Path of the output text file.
-    """
-    pass
 
 
 
@@ -37,5 +40,4 @@ def courses_for_lecturers(json_directory_path, output_json_path):
     """
     pass
 
-names_of_registered_students("semesters_databases/2022a.json", "asd")
 
