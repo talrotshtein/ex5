997cf081f427a5e911b650fef977e995be9ebeca
talrotshtein <talrotshtein@campus.technion.ac.il>
2022-06-29 19:38:53 +0300

    extra tests passed, dubugged problems

--- ex5.py
+++ ex5.py
@@ -2,7 +2,6 @@ import json
 import os
 
 
-
 def names_of_registered_students(input_json_path, course_name):
     names = []
     with open(input_json_path, 'r') as file:
@@ -11,7 +10,6 @@ def names_of_registered_students(input_json_path, course_name):
         if course_name in student['registered_courses']:
             names.append(student['student_name'])
     return names
-
 
 
 def enrollment_numbers(input_json_path, output_file_path):
@@ -24,43 +22,33 @@ def enrollment_numbers(input_json_path, output_file_path):
             if course not in courses:
                 courses[course] = len(names_of_registered_students(input_json_path, course))
     sorted_courses = sorted(courses.keys())
+    i = 0
     for course in sorted_courses:
-        dest.write('"%s" %s\n' % (course, courses[course]))
+        i += 1
+        if i == len(sorted_courses):
+            dest.write('"%s" %s' % (course, courses[course]))
+        else:
+            dest.write('"%s" %s\n' % (course, courses[course]))
     dest.close()
-
-
-
-    """
-    num_of_students = 0
-    course_names = []
-    with open(input_json_path, 'r') as file:
-        source = json.load(file)
-    for course_data in source.values():
-        for course_data['registered_courses'] in course_data:
-             if course_data['registered_courses'] != course_names: #not sure about that
-                course_names.append(course_data['registered_courses'])#not sure about that
-                sorted(course_names)
-                with open(output_file_path, 'w') as wfile:
-                    """
 
 
 def courses_for_lecturers(json_directory_path, output_json_path):
     lecturers_dict = {}
     files = os.listdir(json_directory_path)
     for semester in files:
-        if semester[-1:-6:] != ".json":
+        if semester[-5:] != ".json":
             continue
-        with open(semester, 'r') as file_2:
+        with open(json_directory_path + '/' + semester, 'r') as file_2:
             source = json.load(file_2)
-        for course_id in source.values:
+        for course_id in source.values():
             for lecturer in course_id['lecturers']:
                 if lecturer not in lecturers_dict:
-                    lecturers_dict[lecturer] = course_id['course_name']
+                    lecturers_dict[lecturer] = [course_id['course_name']]
                 else:
                     if course_id['course_name'] not in lecturers_dict[lecturer]:
-                        lecturers_dict[lecturer] += course_id['course_name']
-    with open('myfile.json', 'w') as file_3:
-         json.dump(lecturers_dict, file_3, indent= 4)
+                        lecturers_dict[lecturer].append(course_id['course_name'])
+    with open(output_json_path, 'w') as file_3:
+         json.dump(lecturers_dict, file_3, indent=4)
 
 
 
